0d025f4fb815cd3daee2a7caa73a41feb18ead49
Bogdan Truta <bogdan.truta@firebolt.io>
2025-09-10 16:36:26 +0300

    fix insertPreparedStatementTest

--- src/test/java/com/firebolt/kafka/connect/InsertPreparedStatementTest.java
+++ src/test/java/com/firebolt/kafka/connect/InsertPreparedStatementTest.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 
 import com.firebolt.jdbc.exception.FireboltException;
 import com.firebolt.kafka.connect.reporter.ErrorReporter;
