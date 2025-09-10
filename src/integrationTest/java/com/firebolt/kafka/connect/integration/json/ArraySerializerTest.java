ec2102b41b9b0c9c7550129fd6ed91ea35a512fb
Bogdan Truta <bogdan.truta@firebolt.io>
2025-09-10 16:11:42 +0300

    small fixes and addressing CR

--- src/integrationTest/java/com/firebolt/kafka/connect/integration/json/ArraySerializerTest.java
+++ src/integrationTest/java/com/firebolt/kafka/connect/integration/json/ArraySerializerTest.java
@@ -67,7 +67,6 @@ public class ArraySerializerTest extends BaseIntegrationTest {
             "true,  'WITH null fields included in JSON as field: null'",
             "false, 'WITH null fields omitted from JSON entirely'"
     })
-    @Disabled("Disabled until JDBC driver array type fix FIR-48988")
     void testArraySerialization(boolean includeNulls, String testDescription) throws Exception {
         producer = initializeJsonProducer(includeNulls);
 
