98beed98f1c7679dd9d978f655601f0171e087e5
Bogdan Truta <bogdan.truta@firebolt.io>
2025-10-23 16:19:03 +0300

    address CR and implement TableWriterProvider

--- src/main/java/com/firebolt/kafka/connect/service/AppendOnlyFireboltSinkService.java
+++ src/main/java/com/firebolt/kafka/connect/service/AppendOnlyFireboltSinkService.java
@@ -19,6 +19,7 @@ import java.util.Map;
 import java.sql.SQLException;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import lombok.SneakyThrows;
